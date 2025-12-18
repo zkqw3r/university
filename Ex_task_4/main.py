b8fd005b62ac6e28da083fd56948fb87e374450a
zkqw3r <117681609+zkqw3r@users.noreply.github.com>
2025-12-18 19:17:11 +0300

    Add files via upload

---
+++ Ex_task_4/main.py
@@ -0,0 +1,29 @@
+import re
+import csv
+import urllib.request
+
+url = "https://msk.spravker.ru/avtoservisy-avtotehcentry"
+
+page = urllib.request.urlopen(url)
+text = page.read().decode("utf-8")
+
+pattern = r'(?:class="org-widget-header__title-link"[^>]*>)(?P<Name>[^<]+)</a>(?:.*?class="[^"]*org-widget-header__meta--location"[^>]*>\s*)(?P<Address>[^<]+)</span>(?:.*?Телефон</span></dt>\s*<dd class="spec__value">\s*(?P<Phone>[^<]+)</dd>)?(?:.*?Часы работы</span></dt>\s*<dd class="spec__value">\s*(?P<WorkHours>[^<]+)</dd>)?'
+
+result = re.findall(pattern, text, re.S)
+print(f"Nashlo: {len(result)}")
+
+f = open("result.csv", "w", encoding="utf-8")
+f.write("Name,Address,Phone,Hours\n")
+
+for item in result:
+    n = item[0].strip()
+    a = item[1].strip()
+    p = item[2].strip()
+    h = item[3].strip()
+
+    line = n + "," + a + "," + p + "," + h + "\n"
+
+    f.write(line)
+
+f.close()
+print("готово")
